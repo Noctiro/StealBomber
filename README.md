54f4bb0b8be0ca29f2dff3ddc6ebc80f2c4e41c3
ObcbO <m17089494917@163.com>
2022-03-22 13:03:21 +0800

    轻量化/配置改为Java Properties/修改默认配置的攻击网址

--- README.md
+++ README.md
@@ -13,23 +13,22 @@
 
 | 参数 | 说明 | 例子 |
 | --- | --- | --- |
-| -Dfile= | 自定义文本文件名称(位置) | java -Dfile=../example.txt -jar steal.jar|
+| -Dfile= | 自定义配置名称(位置) | java -Dfile=../example.properties -jar steal.jar|
 
 ## 文本编写
 
-> 默认使用同一文件夹下的文本文件 default.txt(需自行下载: <https://cdn.jsdelivr.net/gh/ObcbO/stealbomb/default.txt>)
+> 默认使用同一文件夹下的配置 default.properties(需自行下载: <https://cdn.jsdelivr.net/gh/ObcbO/stealbomb/default.properties>)
 
-一行只能有 `参数` 和 `参数值` ， 中间使用**空格**隔开(一行只允许一个空格)
+使用Java Properties写法
 
-不允许空行
+一行只能有 `参数` 和 `参数值` ， 中间使用**等号**隔开
 
-| 参数 | 必填 | 默认值 | 说明 |
+| 参数名 | 必填 | 默认值 | 说明 |
 | --- | --- | --- | --- |
-| // | 否 | - | 注释 |
-| 线程数 | 否 | 1 | 并行攻击的线程数量 |
-| 请求方法 | 否 | POST | 支持所有的HTTP请求方法 |
-| 攻击网址 | 是 | - | 发送账号密码的网址 |
-| 参数 | 是 | - | 向网站发送的参数 |
+| threads | 否 | 1 | 并行攻击的线程数量 |
+| method | 否 | POST | 支持所有的HTTP请求方法 |
+| URL | 是 | - | 发送账号密码的网址 |
+| parameter | 是 | - | 向网站发送的参数 |
 
 ## TODO
 
--- src/App.java
+++ src/App.java
@@ -1,7 +1,7 @@
+import java.io.File;
 import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Scanner;
+import java.io.InputStream;
+import java.util.Properties;
 
 public class App {
     // 默认值
@@ -11,6 +11,8 @@ public class App {
     static String param;// 攻击参数
 
     static boolean success = true;
+
+    public static Properties properties;
 
     public static void main(String[] args) {
         try {
@@ -23,89 +25,66 @@ public class App {
     }
 
     private static void file() throws IOException {
-        String str;
-        String first = null;
         String file;
         if (System.getProperty("file") == null || System.getProperty("file") == ""
                 || System.getProperty("file").trim() == "") {
-            file = "default.txt";
-        } else
-            file = System.getProperty("file");
-        Path path = Paths.get(file);
-        Scanner scanner = new Scanner(path);
-        System.out.println("-=-=-=-=- Start reading file");
-        while (scanner.hasNext()) {
-            if (first == null) {
-                str = scanner.next();
-                first = str;
-            } else {
-                str = scanner.next();
-                String[] value = { first, str };
-                first = null;
-                System.out.println(value[0].toString() + " " + value[1].toString());
-                manage(value);
+            if (!new File("src/default.properties").exists()) {
+                generatefile();
             }
-        }
-        scanner.close();
+            file = "default.properties".toString();;
+        } else
+            file = System.getProperty("file").toString();
+        System.out.println(file);
+        InputStream inputStream = App.class.getClassLoader().getResourceAsStream(file);
+        properties = new Properties();
+        properties.load(inputStream);
+        properties.list(System.out);
+        manage();
         System.out.println("-=-=-=-=- File processing completed");
         if (success) {
             post.start(thnum, method, url, param);
         }
     }
 
-    private static void manage(String[] value) {
-        if (value[0] == null) {
+    private static void generatefile() {
+        properties.setProperty("1", "123");
+    }
+
+    private static void manage() {
+        String temp;
+        // 线程数
+        temp = properties.getProperty("threads");
+        if (temp.matches("[0-9]*")) {
+            thnum = Integer.parseInt(temp);
+        } else {
             success = false;
-            System.out.println("ERROR: 无效参数名" + success);
-            return;
+            System.out.println("ERROR: 线程数 你输入的值不是一个正整数");
         }
-        if (value[1] == null && value[0] != "//") {
-            success = false;
-            System.out.println("ERROR: 无效参数值" + success);
-            return;
-        }
-        switch (value[0]) {
-            case "//":
-                break;
-            case "线程数":
-                if (value[1].matches("[0-9]*")) {
-                    thnum = Integer.parseInt(value[1]);
-                } else {
-                    success = false;
-                    System.out.println("ERROR: 线程数 你输入的值不是一个正整数");
-                }
-                break;
-            case "请求方法":
-                String temp = value[1].toUpperCase();
-                switch (temp) {
-                    case "GET":
-                    case "HEAD":
-                    case "POST":
-                    case "PUT":
-                    case "DELETE":
-                    case "CONNECT":
-                    case "OPTIONS":
-                    case "TRACE":
-                    case "PATCH":
-                        url = temp;
-                        break;
-                    default:
-                        System.out.println("ERROR: 请求方法 未知的请求方法");
-                }
-                break;
-            case "攻击网址":
-                if (value[1].matches("^(http|https)://([\\w-]+\\.)+[\\w-]+(/[\\w-./?%&=]*)?$")) {
-                    url = value[1];
-                } else {
-                    success = false;
-                    System.out.println("ERROR: 攻击网址 你输入的字符串不是一个网址");
-                }
-                break;
-            case "参数":
-                param = value[1];
+        // 请求方法
+        temp = properties.getProperty("method").toUpperCase();
+        switch (temp) {
+            case "GET":
+            case "HEAD":
+            case "POST":
+            case "PUT":
+            case "DELETE":
+            case "CONNECT":
+            case "OPTIONS":
+            case "TRACE":
+            case "PATCH":
+                url = temp;
                 break;
             default:
-                System.out.println("ERROR: 未知参数名 " + value[0]);
+                System.out.println("ERROR: 请求方法 未知的请求方法");
         }
+        // 攻击网址
+        if (properties.getProperty("URL").matches("^(http|https)://([\\w-]+\\.)+[\\w-]+(/[\\w-./?%&=]*)?$")) {
+            url = properties.getProperty("URL");
+        } else {
+            success = false;
+            System.out.println("ERROR: 攻击网址 你输入的字符串不是一个网址");
+        }
+        // 参数
+        param = properties.getProperty("parameter");
     }
 }

16ab6b7d753860b1883a153b0527de52d8870d17
ObcbO <49872497+ObcbO@users.noreply.github.com>
2022-04-20 23:50:44 +0800

    测试

--- src/stealbomber/gui/statistics.java
+++ src/stealbomber/gui/statistics.java
@@ -2,10 +2,21 @@ package stealbomber.gui;
 
 import javax.swing.BorderFactory;
 
-public class statistics {
+import org.knowm.xchart.QuickChart;
+import org.knowm.xchart.SwingWrapper;
+import org.knowm.xchart.XYChart;
 
+public class statistics {
     public static void basic() {
         stealbomber.gui.main.statistics.setBorder(BorderFactory.createTitledBorder("统计区"));
+
+        double[] xData = new double[] { 0.0, 1.0, 2.0 };
+        double[] yData = new double[] { 2.0, 1.0, 0.0 };
+
+        // Create Chart
+        XYChart chart = QuickChart.getChart("统计", "时间", "次数", "成功", xData, yData);
+
+        // Show it
+        new SwingWrapper(chart).displayChart();
     }
-    
 }

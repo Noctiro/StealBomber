4d5d64743698ef9509a98593f086bfb2d9376373
ObcbO <49872497+ObcbO@users.noreply.github.com>
2022-04-20 13:32:08 +0800

    内存保护

--- src/stealbomber/manage/out.java
+++ src/stealbomber/manage/out.java
@@ -1,10 +1,16 @@
 package stealbomber.manage;
 
 public class out {
+    static int cs = 0;
+
     public static void println(String string) {
         System.out.println(string);
         if (stealbomber.App.sgui) {
             stealbomber.gui.main.out.setText(stealbomber.gui.main.out.getText() + "\n" + string);
+            cs++;
+            if (cs == 1000) {
+                stealbomber.gui.main.out.setText("");
+            }
         }
     }
 }

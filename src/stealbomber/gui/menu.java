51ade9bc6adbf5c5e9172bbde4eaf13949e3b2d9
ObcbO <49872497+ObcbO@users.noreply.github.com>
2022-04-23 01:08:09 +0800

    更新GUI

--- src/stealbomber/gui/menu.java
+++ src/stealbomber/gui/menu.java
@@ -117,11 +117,26 @@ public class menu {
                         System.out.println("没有找到字符串");
                     } else {
                         int secondi = sb.indexOf("\"", firsti);
-                        System.out.println("发现新版! 版本号:" + sb.substring(firsti, secondi));
                         if (Double.parseDouble(sb.substring(firsti, secondi)) > stealbomber.App.version) {
-                            JOptionPane.showMessageDialog(null, "发现可更新的版本", "检查更新",
-                                    JOptionPane.INFORMATION_MESSAGE,
-                                    null);
+                            System.out.println("发现新版! 版本号:" + sb.substring(firsti, secondi));
+                            int yes = JOptionPane.showConfirmDialog(null, new Object[] {
+                                    "发现可更新的版本!",
+                                    " ",
+                                    "当前版本: " + stealbomber.App.version,
+                                    "最新版本: " + sb.substring(firsti, secondi),
+                            });
+                            if (yes == 0) {
+                                if (Desktop.isDesktopSupported()) {
+                                    Desktop desktop = Desktop.getDesktop();
+                                    try {
+                                        desktop.browse(new URI("https://github.com/obcbo/stealbomber/releases/latest"));
+                                    } catch (IOException | URISyntaxException e1) {
+                                        e1.printStackTrace();
+                                    }
+                                } else {
+                                    JOptionPane.showInputDialog(null, "更新地址", "https://github.com/obcbo/stealbomber/releases/latest");
+                                }
+                            }
                         } else {
                             JOptionPane.showMessageDialog(null, "当前已最新版本", "检查更新",
                                     JOptionPane.INFORMATION_MESSAGE,

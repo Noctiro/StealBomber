b243161a40de09ba3bc89e2cccf2fb4b44a7f7fa
ObcbO <49872497+ObcbO@users.noreply.github.com>
2022-04-25 13:43:16 +0800

    样式更新

--- src/stealbomber/manage/update.java
+++ src/stealbomber/manage/update.java
@@ -1,9 +1,11 @@
 package stealbomber.manage;
 
-import java.awt.BorderLayout;
 import java.awt.Desktop;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -59,36 +61,78 @@ public class update implements Runnable {
             if (Double.parseDouble(version) > stealbomber.App.version) {
                 System.out.println("发现新版! 版本号:" + version);
                 System.out.println("下载地址: " + downloadlink);
-                System.out.println(info);
-                updatedialog();
+                if (stealbomber.App.sgui) {
+                    updatedialog();
+                }
             } else {
-                JOptionPane.showMessageDialog(null, "当前已最新版本", "检查更新",
-                        JOptionPane.INFORMATION_MESSAGE,
-                        null);
+                if (stealbomber.App.sgui) {
+                    JOptionPane.showMessageDialog(null, "当前已最新版本", "检查更新",
+                            JOptionPane.INFORMATION_MESSAGE,
+                            null);
+                }
             }
         } catch (Exception e1) {
             e1.printStackTrace();
-            JOptionPane.showMessageDialog(null, "检查更新失败", "错误", 0);
+            out.warn("ERROR: 无法连接到 api.github.com 以检查更新");
         }
     }
 
     private void updatedialog() {
-        final JDialog dialog = new JDialog(stealbomber.gui.main.jf, "发现新版本", true);
+        final JDialog dialog = new JDialog(stealbomber.gui.main.jf, "检查更新", true);
         dialog.setSize(400, 350);
         dialog.setResizable(false);
         dialog.setLocationRelativeTo(stealbomber.gui.main.jf);
 
+        
+        // GridBagLayout
+        GridBagLayout cp = new GridBagLayout(); // 实例化布局对象
+        dialog.setLayout(cp); // jf窗体对象设置为GridBagLayout布局
+        GridBagConstraints gbc = new GridBagConstraints();// 实例化这个对象用来对组件进行管理
+        // NONE：不调整组件大小。
+        // HORIZONTAL：加宽组件，使它在水平方向上填满其显示区域，但是不改变高度。
+        // VERTICAL：加高组件，使它在垂直方向上填满其显示区域，但是不改变宽度。
+        // BOTH：使组件完全填满其显示区域。
+
+        gbc.insets = new Insets(2, 2, 2, 2);// top left bottom right
+
         JPanel panel = new JPanel();
         dialog.setContentPane(panel);
-        dialog.setLayout(new BorderLayout(5, 5));
 
-        JLabel tversion = new JLabel("版本: " + stealbomber.App.version + "->" + version);
+        JLabel text = new JLabel("<html><h1>发现新版本</h1></html>");
+        gbc.weightx = 10;// 第一列分布方式为10%
+        gbc.fill = GridBagConstraints.BOTH;
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.gridwidth = 1;
+        gbc.gridheight = 2;
+        cp.setConstraints(text, gbc);
+
+        JLabel tversion = new JLabel("<html>版本: <font color=\"red\">" + stealbomber.App.version + "</font> -> <font color=\"green\">" + version + "</font></html>");
+        gbc.weightx = 10;// 第一列分布方式为10%
+        gbc.fill = GridBagConstraints.BOTH;
+        gbc.gridx = 0;
+        gbc.gridy = 3;
+        gbc.gridwidth = 1;
+        gbc.gridheight = 1;
+        cp.setConstraints(tversion, gbc);
 
         JTextArea ainfo = new JTextArea(15, 15);
         ainfo.setText(info);
         JScrollPane tinfo = new JScrollPane(ainfo);
+        gbc.weightx = 10;// 分布方式为10%
+        gbc.fill = GridBagConstraints.BOTH;
+        gbc.gridx = 0;
+        gbc.gridy = 4;
+        gbc.gridwidth = 1;
+        gbc.gridheight = 5;
+        cp.setConstraints(tinfo, gbc);
 
         JButton download = new JButton("下载");
+        gbc.gridx = 1;
+        gbc.gridy = 6;
+        gbc.gridwidth = 1;
+        gbc.gridheight = 1;
+        cp.setConstraints(download, gbc);
         download.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -106,6 +150,11 @@ public class update implements Runnable {
         });
 
         JButton cancel = new JButton("取消");
+        gbc.gridx = 3;
+        gbc.gridy = 6;
+        gbc.gridwidth = 1;
+        gbc.gridheight = 1;
+        cp.setConstraints(cancel, gbc);
         cancel.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -114,10 +163,11 @@ public class update implements Runnable {
             }
         });
 
-        panel.add(tversion, BorderLayout.WEST);
-        panel.add(tinfo, BorderLayout.EAST);
-        panel.add(download, BorderLayout.SOUTH);
-        panel.add(cancel, BorderLayout.SOUTH);
+        panel.add(text);
+        panel.add(tversion);
+        panel.add(tinfo);
+        panel.add(download);
+        panel.add(cancel);
         dialog.setVisible(true);
     }
 }

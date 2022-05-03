80834f8b4c2d28732475cc173a6499fcc2aa1ce0
ObcbO <49872497+ObcbO@users.noreply.github.com>
2022-05-04 00:28:28 +0800

    优化

--- src/stealbomber/attack/password.java
+++ src/stealbomber/attack/password.java
@@ -6,16 +6,21 @@ public class password {
     private static Random random = new Random();
     private static StringBuilder pass;
 
+    private static final String all = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*";
+    private static final String bletter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final String sletter = "abcdefghijklmnopqrstuvwxyz";
+    private static final String num = "0123456789";
+    private static final String spcial = "!@#$%^&*";
+
     protected static String get() {
         pass = new StringBuilder();
         int extent = random.nextInt(16 - 8) + 8;
         switch (random.nextInt(10)) {
             // 全随机
             case 0:
-                final String str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*";
                 for (byte i = 0; i < extent; i++) {
-                    int randomInt = random.nextInt(str.length());
-                    pass.append(str.charAt(randomInt));
+                    int randomInt = random.nextInt(all.length());
+                    pass.append(all.charAt(randomInt));
                 }
                 break;
             // 弱口令
@@ -91,41 +96,37 @@ public class password {
     }
 
     private static void bletter(int length) {
-        final String str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         for (byte i = 0; i < length; i++) {
-            int randomInt = random.nextInt(str.length());
-            pass.append(str.charAt(randomInt));
+            int randomInt = random.nextInt(bletter.length());
+            pass.append(bletter.charAt(randomInt));
         }
     }
 
     private static void sletter(int length) {
-        final String str = "abcdefghijklmnopqrstuvwxyz";
         for (byte i = 0; i < length; i++) {
-            int randomInt = random.nextInt(str.length());
-            pass.append(str.charAt(randomInt));
+            int randomInt = random.nextInt(sletter.length());
+            pass.append(sletter.charAt(randomInt));
         }
     }
 
     private static void num(int length) {
-        final String str = "0123456789";
         for (byte i = 0; i < length; i++) {
-            int randomInt = random.nextInt(str.length());
-            pass.append(str.charAt(randomInt));
+            int randomInt = random.nextInt(num.length());
+            pass.append(num.charAt(randomInt));
         }
     }
 
     private static void ms() {
-        // 20%的概率
+        // 25%的概率
         if (random.nextInt(19) == 0) {
             spcial(1);
         }
     }
 
     private static void spcial(int length) {
-        final String str = "!@#$%^&*";
         for (byte i = 0; i < length; i++) {
-            int randomInt = random.nextInt(str.length());
-            pass.append(str.charAt(randomInt));
+            int randomInt = random.nextInt(spcial.length());
+            pass.append(spcial.charAt(randomInt));
         }
     }
 }

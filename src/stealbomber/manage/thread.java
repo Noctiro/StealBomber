21100528f4183216c32b0c2c1d9e79f2d1c98af3
ObcbO <49872497+ObcbO@users.noreply.github.com>
2022-05-20 19:29:31 +0800

    回救错误线程

--- src/stealbomber/manage/thread.java
+++ src/stealbomber/manage/thread.java
@@ -2,7 +2,13 @@ package stealbomber.manage;
 
 public class thread {
     public static boolean on = false;
-    private static ThreadGroup gat = new ThreadGroup("GroupAttack");
+    private static ThreadGroup gat = new ThreadGroup("GroupAttack") {
+        public void uncaughtException(Thread t, Throwable e) {// 当线程抛出问题时执行
+            System.out.println(t.getName() + ": " + e.getMessage());
+            System.out.println("开始重启 " + t.getName() + " 线程");
+            new Thread(gat, new stealbomber.attack.main(), t.getName()).start();
+        }
+    };
 
     public static void start() {
         on = true;

6b9d92265fb134d9b618c75ed5f37e77d8e81614
ObcbO <49872497+ObcbO@users.noreply.github.com>
2022-05-23 00:20:18 +0800

    精简

--- src/stealbomber/attack/password.java
+++ src/stealbomber/attack/password.java
@@ -109,8 +109,7 @@ public class Password {
 
     private static void ms() {
         // 25%的概率
-        int probability = ThreadLocalRandom.current().nextInt(19);
-        if (probability == 0) {
+        if (ThreadLocalRandom.current().nextInt(19) == 0) {
             g(3, 1);
         }
     }

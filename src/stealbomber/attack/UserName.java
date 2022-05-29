06716007c134d804b5921dfdbf91b2404037af67
ObcbO <49872497+ObcbO@users.noreply.github.com>
2022-05-29 22:06:00 +0800

    减少计算

--- src/stealbomber/attack/UserName.java
+++ src/stealbomber/attack/UserName.java
@@ -12,31 +12,31 @@ public class UserName {
 
     protected static String get() {
         StringBuilder username = new StringBuilder();
-        try {
-            switch (ThreadLocalRandom.current().nextInt(2)) {
-                case 2:
+        switch (ThreadLocalRandom.current().nextInt(2)) {
+            case 2:
+                try {
                     username.append(Storage.NUMSEG[ThreadLocalRandom.current().nextInt(NUMSEGL)]);
-                    for (byte i = 0; i < 8; i++) {
-                        username.append(ThreadLocalRandom.current().nextInt(10));
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    return get();
+                }
+                for (byte i = 0; i < 8; i++) {
+                    username.append(ThreadLocalRandom.current().nextInt(10));
+                }
+                break;
+            case 1:
+                username.append("@qq.com");
+            case 0:
+            default:
+                int length = ThreadLocalRandom.current().nextInt(8, 13);
+                for (byte i = 0; i < length; i++) {
+                    if (i == 0) {
+                        username.insert(0, ThreadLocalRandom.current().nextInt(1, 10));
+                    } else {
+                        username.insert(0, ThreadLocalRandom.current().nextInt(10));
                     }
-                    break;
-                case 1:
-                    username.append("@qq.com");
-                case 0:
-                default:
-                    int length = ThreadLocalRandom.current().nextInt(8, 13);
-                    for (byte i = 0; i < length; i++) {
-                        if (i == 0) {
-                            username.insert(0, ThreadLocalRandom.current().nextInt(1, 10));
-                        } else {
-                            username.insert(0, ThreadLocalRandom.current().nextInt(10));
-                        }
-                    }
-                    break;
-            }
-            return username.toString();
-        } catch (ArrayIndexOutOfBoundsException e) {
-            return get();
+                }
+                break;
         }
+        return username.toString();
     }
 }

